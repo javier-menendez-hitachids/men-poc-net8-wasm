4bea2c2e134cca394a8c8a39e8d3471736197afa
Javier Menéndez <javier.menendez@hitachivantara.com>
2024-12-12 13:43:22 +0000

    WebAssembly + Contentful example

---
+++ Models/CreditCard.cs
@@ -0,0 +1,9 @@
+﻿namespace MenulioPocWasm.Models
+{
+    public class CreditCard
+    {
+        public string? Issuer { get; set; }
+        public int Fee { get; set; }
+        public string? Type { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MenulioPocWasm;
+using Contentful.Core.Configuration;
+using Contentful.Core;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add Contentful client
+builder.Services.AddScoped<IContentfulClient>(sp =>
+{
+    var httpClient = sp.GetRequiredService<HttpClient>();
+    var options = new ContentfulOptions
+    {
+        DeliveryApiKey = "0HeeMkqQH9tkxxq6LtRuGyvXwAKKWHgMlM0pMWv5NzA",
+        SpaceId = "j3vmcasldixe",
+        PreviewApiKey = "zZeHGwlP9Xe2wckGwYzoei7a89X5kJdB0yKLi-qLLZo", // Optional
+        Environment = "master" // Optional, defaults to "master"
+    };
+    return new ContentfulClient(httpClient, options);
+});
 
 await builder.Build().RunAsync();
